8f15a8b237ac6f639e7f5e3c54554b7f4518f407
Shoya Kai <s-kai@lumber-mill.co.jp>
2017-07-20 18:48:04 +0900

    写真撮影が可能なときにLEDを点灯するようにしました

--- bin/take_picture.py
+++ bin/take_picture.py
@@ -5,6 +5,7 @@ import time
 import RPi.GPIO as GPIO
 import os
 import context
+import sys
 
 #ローカルのディレクトリ存在確認(なければ作成)
 if not os.path.isdir(context.LOCAL_PATH+context.SAVE_DIRECTORY):
@@ -12,17 +13,25 @@ if not os.path.isdir(context.LOCAL_PATH+context.SAVE_DIRECTORY):
 
 # タクトスイッチPIN番号
 TACT_SWITCH = 14
+LED = 15
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(TACT_SWITCH, GPIO.IN)
+GPIO.setup(LED, GPIO.OUT)
 
 try:
     while True:
+        GPIO.output(LED, GPIO.HIGH)
         if GPIO.input(TACT_SWITCH) == GPIO.HIGH:
+            # タクトスイッチON
+            GPIO.output(LED, GPIO.LOW)
+            print "take a picture ..",
+            sys.stdout.flush()
             context.take_picture()
-            print "take a picture."
+            print "ok"
         else:
             pass
 except:
-    print "error."
-GPIO.cleanup()
+    print sys.exc_info()
+finally:
+    GPIO.cleanup()
